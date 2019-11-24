2f71cc147f644f10f3451ab7c267a25ada551e92
Antonio Vargas <15042864+szego@users.noreply.github.com>
2019-11-23 23:24:19 -0800

    0.1.1 update
    - Fixed road, water order. Roads now render above water.
    - Fixed a typo in the oregon_mask() documentation.
    - Updated version to 0.1.1.

--- R/oregon_mask.R
+++ R/oregon_mask.R
@@ -7,7 +7,7 @@
 #' * `road_color`: The color of the road lines. When set to NA (the default),
 #'   roads are not rendered. Set this to any color to include roads in the plot.
 #' * `water_color`: The color of the water. When set to NA (the default), water
-#'   is not rendered. Set this to any color to include roads in the plot. Water
+#'   is not rendered. Set this to any color to include water in the plot. Water
 #'   may take quite a while to render.
 #' * `bg_color`: The color of the mask surrounding Oregon.
 #' * `xlim`: Limits of the x-axis. Defaults to include all of Oregon.
@@ -39,16 +39,6 @@ oregon_mask <- function(
         )
     )
 
-    if(!is.na(water_color))
-        p <- c(
-            ggplot2::geom_sf(
-                data = or_water_bodies,
-                fill = water_color,
-                color = NA
-            ),
-            p
-        )
-
     if(!is.na(road_color))
         p <- c(
             ggplot2::geom_sf(
@@ -56,6 +46,16 @@ oregon_mask <- function(
                 color = road_color,
                 fill = NA,
                 size = road_size
+            ),
+            p
+        )
+
+    if(!is.na(water_color))
+        p <- c(
+            ggplot2::geom_sf(
+                data = or_water_bodies,
+                fill = water_color,
+                color = NA
             ),
             p
         )
